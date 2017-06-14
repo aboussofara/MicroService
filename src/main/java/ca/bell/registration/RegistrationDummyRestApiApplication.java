5eee088e3379e159b3a9edf716d80a59d935c311
Simon Francoeur <simon.francoeur@bellmedia.ca>
2017-06-14 13:46:06 -0400

    Init projet

---
+++ src/main/java/ca/bell/registration/RegistrationDummyRestApiApplication.java
@@ -0,0 +1,12 @@
+package ca.bell.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationDummyRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationDummyRestApiApplication.class, args);
+	}
+}
