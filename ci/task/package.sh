aca9c7d00c053307802866ca0f526501623bf6ca
aboussofara <amine.boussofara@bellmedia.ca>
2017-07-19 16:06:37 -0400

    Added maven config 2

--- ci/task/package.sh
+++ ci/task/package.sh
@@ -4,5 +4,6 @@ set -e -u -x
 
 cd source-code/
 set MAVEN_OPTS=-Dhttp.proxyHost=142.117.128.44 -Dhttp.proxyPort=8083 -Dhttps.proxyHost=142.117.128.44 -Dhttps.proxyPort=8083
-env
+wget google.com
+cat index.html
 ./mvnw package
