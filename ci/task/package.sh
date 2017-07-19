3ccff59941ccc9d2e065b66677173041e6d3ff9e
aboussofara <amine.boussofara@bellmedia.ca>
2017-07-19 15:29:36 -0400

    Added maven config

--- ci/task/package.sh
+++ ci/task/package.sh
@@ -3,6 +3,6 @@
 set -e -u -x
 
 cd source-code/
-export MAVEN_OPTS=-Dhttp.proxyHost=fastweb.int.bell.ca -Dhttp.proxyPort=8083 -Dhttps.proxyHost=fastweb.int.bell.ca -Dhttps.proxyPort=8083
+set MAVEN_OPTS=-Dhttp.proxyHost=fastweb.int.bell.ca -Dhttp.proxyPort=8083 -Dhttps.proxyHost=fastweb.int.bell.ca -Dhttps.proxyPort=8083
 env
 ./mvnw package
